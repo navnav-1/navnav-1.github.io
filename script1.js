6448644abb584a5ce13f5fdf8133fab5ad5a6a78
navnav-1 <navdeep4321lakhia@gmail.com>
2025-10-26 08:10:23 -0400

    script.js for the Portfolio Website

---
+++ script1.js
@@ -0,0 +1,19 @@
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "300px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+
+//HTML scroll effect//
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+});
